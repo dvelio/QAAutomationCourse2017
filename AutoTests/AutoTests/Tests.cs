303062f4c34690b00f30d0826b93d194734d49b6
Jeni Milanova <jmilanova@melontech.com>
2017-04-03 09:20:00 +0300

    First commit

---
+++ AutoTests/AutoTests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace MyTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com");
+            driver.FindElement(By.ClassName("map-search"))
+                .Click();
+            driver.FindElement(By.ClassName("dropdown-trigger"))
+                .Click();
+            Thread.Sleep(3000);
+            driver.Close();
+            driver.Dispose();
+        }
+    }
+}
