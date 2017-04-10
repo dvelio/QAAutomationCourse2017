d429bb7f09ec04823d3868768cb1f51a182e5cf8
Tsanka <tsstrelkova@melontech.com>
2017-04-10 10:35:50 +0300

    QA Lesson2 - Home work

--- AutoTests/AutoTests/Tests.cs
+++ AutoTests/AutoTests/Tests.cs
@@ -9,17 +9,32 @@ namespace MyTest
     public class Tests
     {
         [Test]
-        public void Test1()
+        //public void Test1()
+        //{
+        //    IWebDriver driver = driver = new ChromeDriver();
+        //    driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com");
+        //    driver.FindElement(By.ClassName("map-search"))
+        //        .Click();
+        //     driver.FindElement(By.ClassName("dropdown-trigger"))
+        //        .Click();
+        //     Thread.Sleep(3000);
+        //     driver.Close();
+        //    driver.Dispose();
+        //}
+
+        public void YavlenaFirstTestToFindElementsByClassName()
         {
             IWebDriver driver = driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com");
-            driver.FindElement(By.ClassName("map-search"))
-                .Click();
-            driver.FindElement(By.ClassName("dropdown-trigger"))
-                .Click();
-            Thread.Sleep(3000);
+            driver.FindElement(By.ClassName("map-search")).Click();
+            Thread.Sleep(4000);
+            driver.FindElement(By.ClassName("icon-rent")).Click();
+            Thread.Sleep(4000);
+            driver.FindElement(By.ClassName("clear-btn")).Click();
+            Thread.Sleep(4000);
             driver.Close();
             driver.Dispose();
         }
     }
+   
 }
