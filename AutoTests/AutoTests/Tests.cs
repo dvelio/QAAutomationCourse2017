4c027b1c4c1807fbabc0087f844daa982cf8e2c6
Evelina <edukova@melontech.com>
2017-05-04 09:51:25 +0300

    Homework3EDCssSelectors

--- AutoTests/AutoTests/Tests.cs
+++ AutoTests/AutoTests/Tests.cs
@@ -128,5 +128,29 @@ namespace MyTest
             driver.Close();
             driver.Quit();
         }
+        [Test]
+        public void Homework3EDCssSelectors()
+        //Selected number 10125
+        // 1. Opens the url
+        // 2. Clicks on the respective Услуга, Тип имот, Вид имот, Област,  град depending on your number and choice of City
+        // 3. Click on “Свържи се с брокер” button for the (preferably) second property in the list
+        // Commit and push the test
+        {
+            IWebDriver driver = driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com/propertylist");
+            Thread.Sleep(3000);
+            driver.FindElement(By.CssSelector("[href *= '/propertylist/sales/']")).Click();
+            driver.FindElement(By.XPath("//form/p[2]/a[1]")).Click();
+            driver.FindElement(By.XPath("/html/body/div2/div/section/div/div/div/div/form/p[2]/a[1]")).Click();
+            driver.FindElement(By.XPath("//form/p[4]/a[2]")).Click();
+            driver.FindElement(By.XPath("//*[contains(text(),'к.к. Слънчев бряг')]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.ClassName("icheckbox_flat-green")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.ClassName("icon - search____ICON")).Click();
+            driver.Close();
+            driver.Quit();
+        }
+
     }
 }
