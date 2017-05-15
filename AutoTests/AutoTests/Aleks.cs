d3ecfb9f2b6ec3a0a7db2d9a558d04c7c1bb0340
Jeni Milanova <jmilanova@melontech.com>
2017-05-15 10:59:56 +0300

    QA: added separate Class for each member

---
+++ AutoTests/AutoTests/Aleks.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace AutoTests
+{
+    [TestFixture]
+    public class Aleks
+    {
+    }
+}
