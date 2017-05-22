0fe9ff352db7fd75915335fc629f5be55422cbd7
Dimitar Velev <mitko@melontech.com>
2017-05-22 08:55:58 +0300

    - Moved test cases to Mitko.cs  - added implicit waits instead of thread sleep  - added maximise browser to all cases

--- AutoTests/AutoTests/Mitko.cs
+++ AutoTests/AutoTests/Mitko.cs
@@ -13,5 +13,58 @@ namespace AutoTests
     [TestFixture]
     public class Mitko
     {
+        [Test]
+        public void HW1Mitko()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com");
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.ClassName("ui-autocomplete-input")).SendKeys("Pavlovo");
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.ClassName("icon-search____ICON")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.Id("searchBox-validation"));
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.ClassName("ui-autocomplete-input")).Clear();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.ClassName("ui-autocomplete-input")).SendKeys("Павлово");
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.Id("ui-id-2")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.ClassName("icon-search____ICON")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.ClassName("brand")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.ClassName("map-search")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.Close();
+            driver.Quit();
+        }
+
+        [Test]
+        public void HW3Mitko()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com/propertylist");
+            driver.FindElement(By.CssSelector("a.offer_filter_btn:nth-of-type(1)")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.CssSelector("a.offer_filter_btn:nth-of-type(4)")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.XPath("//form/p[3]/a[3]")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.XPath("//form/p[4]/a[7]")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.XPath("//*[text()='Парцел']")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.XPath("//form/p[5]/a[98]")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.FindElement(By.XPath("//*[text()='Свържи се с брокер']")).Click();
+            driver.Manage().Timeouts().ImplicitlyWait(System.TimeSpan.FromSeconds(10));
+            driver.Close();
+            driver.Quit();
+        }
     }
 }
