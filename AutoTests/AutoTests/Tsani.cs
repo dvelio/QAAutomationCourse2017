7d894df6109d1d31eb7b61a4562a2218a1a56ebc
Tsanka <tsstrelkova@melontech.com>
2017-06-04 22:37:19 +0300

    HomeWorkAsserts

--- AutoTests/AutoTests/Tsani.cs
+++ AutoTests/AutoTests/Tsani.cs
@@ -258,6 +258,7 @@ namespace AutoTests
 
         }
 
+        
 
         [Test]
 
@@ -269,12 +270,14 @@ namespace AutoTests
         [TestCase("https://yavlenawebsite.melontech.com/propertylist/", ExpectedResult = true)]
         [TestCase("https://yavlenawebsite.melontech.com/service/dealhelp", ExpectedResult = true)]
 
+       
+
 
         public Boolean Lesson5HomeWork(string urlToOpen)
         {
 
 
-            bool elementsDisplayed = false;
+            bool elementsDisplayed = true;
             Console.WriteLine("Test of :"+'\n'+urlToOpen);
             Console.WriteLine("Result:");
             IWebDriver driver = new ChromeDriver();
@@ -314,60 +317,61 @@ namespace AutoTests
 
             // Контакти - <a href="/contact">Контакти</a> 
             IWebElement contactElement = driver.FindElement(By.CssSelector("a[href='/contact']"));
-           
+
+
 
             if (salesElement.Displayed)
-            {
-                if (rentalsElement.Displayed)
-                {
-                    if (serviceElement.Displayed)
-                    {
-                        if (faqElement.Displayed)
-                        {
-                            if (aboutElement.Displayed)
-                            {
-                                if (brokerElement.Displayed)
-                                {
-                                    if (contactElement.Displayed)
-                                    {
-                                        elementsDisplayed = true;
-                                    }
-                                    else
-                                    {
-                                        throw new Exception("Button Contact is not displayed");
-                                    }
-                                }
-                                else
-                                {
-                                    throw new Exception("Button Broker is not displayed");
-                                }                
-                            }
-                            else
-                            {
-                                throw new Exception("Button Faq is not displayed!");
-                            }
-                        }
-                        else
-                        {
-                            throw new Exception("Button Faq is not displayed!");
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("Button Service is not displayed!");
-                    }
-                }
-                else
-                {
-                    throw new Exception("Button Rentals on is not diplayed!");
-                }
+             {
+                 if (rentalsElement.Displayed)
+                 {
+                     if (serviceElement.Displayed)
+                     {
+                         if (faqElement.Displayed)
+                         {
+                             if (aboutElement.Displayed)
+                             {
+                                 if (brokerElement.Displayed)
+                                 {
+                                     if (contactElement.Displayed)
+                                     {
+                                         elementsDisplayed = true;
+                                     }
+                                     else
+                                     {
+                                         throw new Exception("Button Contact is not displayed");
+                                     }
+                                 }
+                                 else
+                                 {
+                                     throw new Exception("Button Broker is not displayed");
+                                 }                
+                             }
+                             else
+                             {
+                                 throw new Exception("Button Faq is not displayed!");
+                             }
+                         }
+                         else
+                         {
+                             throw new Exception("Button Faq is not displayed!");
+                         }
+                     }
+                     else
+                     {
+                         throw new Exception("Button Service is not displayed!");
+                     }
+                 }
+                 else
+                 {
+                     throw new Exception("Button Rentals on is not diplayed!");
+                 }
 
-            }
-            else
-            {
-                throw new Exception("Button Sales is not displayed");
-            }
-        
+             }
+             else
+             {
+                 throw new Exception("Button Sales is not displayed");
+             }
+            
 
 
             Thread.Sleep(5000);
@@ -382,16 +386,261 @@ namespace AutoTests
 
         }
 
+
+        [Test]
+
+        //Choose at least 5 pages, and on each of them check all the Header links are present
+        //Assert is used 
+
+        [TestCase("https://yavlenawebsite.melontech.com/career")]
+        [TestCase("https://yavlenawebsite.melontech.com/service/sale")]
+        [TestCase("https://yavlenawebsite.melontech.com/service/forrent")]
+        [TestCase("https://yavlenawebsite.melontech.com/propertylist/")]
+        [TestCase("https://yavlenawebsite.melontech.com/service/dealhelp")]
+
+
+
+
+        public void Lesson5HomeWorkWithAssert(string urlToOpen)
+        {
+
+
+           
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(urlToOpen);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            // match substring as prefix
+            IWebElement cookiesBut = driver.FindElement(By.CssSelector("*[class^='hide-cookies-message']"));
+            cookiesBut.Click();
+
+            // Продажба - <a href="/properties/sales/София (столица)/София/София/София" data-container="PropertySale">Продажба</a>
+            IWebElement salesElement = driver.FindElement(By.CssSelector("a[href^='/properties/sales/София']"));
+
+
+
+            // Наем - <a href="/properties/rentals/София (столица)/София/София/София" data-container="GiveARent">Наем</a>
+            IWebElement rentalsElement = driver.FindElement(By.CssSelector("a[href^='/properties/rentals/']"));
+
+
+            // Услуги - <a href="/service">Услуги</a>
+            IWebElement serviceElement = driver.FindElement(By.CssSelector("a[href='/service']"));
+
+
+            // Въпроси - <a href="/faq">Въпроси</a>
+            IWebElement faqElement = driver.FindElement(By.CssSelector("a[href='/faq']"));
+
+
+            // За нас - <a href="/about">За нас</a>
+            IWebElement aboutElement = driver.FindElement(By.CssSelector("a[href='/about']"));
+
+
+            // Брокери - <a href="/broker">Брокери</a>
+            IWebElement brokerElement = driver.FindElement(By.CssSelector("a[href='/broker']"));
+
+
+            // Контакти - <a href="/contact">Контакти</a> 
+            IWebElement contactElement = driver.FindElement(By.CssSelector("a[href='/contact']"));
+
+            bool allElementsShown = salesElement.Displayed && rentalsElement.Displayed && serviceElement.Displayed && faqElement.Displayed && aboutElement.Displayed && brokerElement.Displayed  && contactElement.Displayed;
+
+            Assert.AreEqual(true, allElementsShown, "Elements - Продажба , Наем, Услуги, Въпроси, За нас, Брокери, Контакти are not displayed!");
+
+
+            //TestCleanUp
+            DriverCleanUp(driver);
+           
+
+        }
+
+        [Test]
+        public void Lesson5HomeWorkItIsExtraTaskFromLesson3ImplementedWithAssert()
+        {
+
+            //1.Open https://yavlenawebsite.melontech.com/broker/
+
+            //2.Click on the „зареди още“ button.
+            //This way all the brokers are listed
+
+            //3.For each broker check that if you type their name in the Search field, only one broker is listed – the one you are searching for
+
+
+
+            //1.Open https://yavlenawebsite.melontech.com/broker/
+            string url = "https://yavlenawebsite.melontech.com/broker/";
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            // match substring as prefix
+            IWebElement cookiesBut = driver.FindElement(By.CssSelector("*[class^='hide-cookies-message']"));
+            cookiesBut.Click();
+
+            //2.Click on the „зареди още“ button.
+            //This way all the brokers are listed
+            Thread.Sleep(6000);
+
+
+            IWebElement moreResults = driver.FindElement(By.PartialLinkText("Зареди"));
+            Assert.That(moreResults.Displayed, Is.True, "Button Зареди s not displayed!");
+            moreResults.Click();
+
+            //3.For each broker check that if you type their name in the Search field, only one broker is listed – the one you are searching for
+
+            Thread.Sleep(3000);
+            // find all  brokers and put in a list
+            IList<IWebElement> moreBrokers = driver.FindElements(By.CssSelector("h3.name"));
+
+            // all names in array
+            String[] allBrokersText = new String[moreBrokers.Count];
+
+            for (int i = 0; i < moreBrokers.Count; i++)
+            {
+                allBrokersText[i] = moreBrokers[i].Text;
+                
+            }
+            //  search by name and check result - loop 
+            IWebElement searchByName = driver.FindElement(By.ClassName("input-search"));
+
+            for (int j = 0; j < allBrokersText.Length; j++)
+            {
+                searchByName.Clear();
+                searchByName.SendKeys(allBrokersText[j]);
+                Thread.Sleep(2000);
+                // check if just one result of the search is listed
+                IList<IWebElement> namesFound = driver.FindElements(By.CssSelector(".name"));
+                // no results
+                Assert.That(namesFound.Count, Is.GreaterThan(0), "No results about " + allBrokersText[j]);
+                // more brokers
+                Assert.That(namesFound.Count, Is.EqualTo(1), "More than one broker found");
+                // compare name - input field and listed as result of the search 
+                StringAssert.AreEqualIgnoringCase(allBrokersText[j], driver.FindElement(By.CssSelector(".name")).Text,"Different names");
+                Thread.Sleep(2000);
+            }
+            
+
+            //TestCleanUp
+            DriverCleanUp(driver);
+
+
+        }
+
+        [Test]
+        //Create a test to check that Yavlena logo is present both on Desktop and Mobile view.
+        //Hint: use TestCase attribute to pass the test as argument the window size.Use
+        // driver.Manage().Window.Size = new System.Drawing.Size(300, 968); 
+        // to resize your window
+        [TestCase(360,640)]
+        [TestCase(1900,940)]
+        
+
+        public void YavlenaLogo(int X, int Y)
+        {
+
+            Assert.That(new int[] { X, Y }, Has.All.Positive, "Dimensions are not  positive numbers!"); 
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com");
+            driver.Manage().Window.Size = new System.Drawing.Size(X,Y);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            // cookies accept
+            IWebElement cookiesBut = driver.FindElement(By.CssSelector("*[class^='hide-cookies-message']"));
+            cookiesBut.Click();
+
+
+            // Yavlena Logo
+            IWebElement logoElement = driver.FindElement(By.CssSelector("div.brand > a[href='/'"));          
+            Assert.That(logoElement.Displayed,Is.True, "Logo is not displayed in size " + X + " x " + Y);
+
+            //TestCleanUp
+            DriverCleanUp(driver);
+
+            Thread.Sleep(6000);
+
+
+        }
+
+
+        [Test]
+        // Create a test to check element that is changing from Desktop to Mobile view
+        // Create a test to click on the “Продажба” link in the header of the website.The test should be valid for both Desktop and Mobile view
+        [TestCase(600, 800)]
+        [TestCase(1900, 940)]
+        
+
+        public void YavlenaSells(int X, int Y)
+        {
+            Assert.That(new int[] { X, Y }, Has.All.Positive, "Dimensions are not  positive numbers!");
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://yavlenawebsite.melontech.com");
+            driver.Manage().Window.Size = new System.Drawing.Size(X, Y);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            // match substring as prefix
+            IWebElement cookiesBut = driver.FindElement(By.CssSelector("*[class^='hide-cookies-message']"));
+            cookiesBut.Click();
+            Thread.Sleep(1000);
+
+
+            // In case of mobile view, user should click on the hamburger menu button
+            if (X < 1200)
+            {
+                Console.WriteLine("Mobile view testing");
+                
+                // Mobile Menu 
+                IWebElement mobileMenuElement = driver.FindElement(By.CssSelector("div.mobile-nav-trigger > a"));
+                Assert.That(mobileMenuElement.Displayed, Is.True, "Hamburher Menu button is not displayed in Mobile view!");
+                mobileMenuElement.Click();
+                Thread.Sleep(1000);
+
+
+                // Продажба 
+                IWebElement salesElement = driver.FindElement(By.CssSelector("a[href^='/properties/sales/']"));
+                Assert.That(salesElement.Displayed, Is.True, "Продажби is not displayed in Mobile view of site!");
+                salesElement.Click();
+
+            }
+            else
+            {
+                Console.WriteLine("Desktop view testing");
+                // Продажба 
+                IWebElement salesDescElement = driver.FindElement(By.CssSelector("a[href^='/properties/sales/']"));
+                Assert.That(salesDescElement.Displayed, Is.True, "Продажби is not displayed in Desktop view of site!");
+                salesDescElement.Click();
+
+            }
+
+            //TestCleanUp
+            DriverCleanUp(driver);
+
+        }
+
+        public static void FindByAndClick(IWebDriver myDriver, By byLocator)
+        {
+            IWebElement cssElement = myDriver.FindElement(byLocator);
+            cssElement.Click();
+
+        }
+
         public static void FindByCssLocatorAndClick(IWebDriver myDriver, string strToFindBy)
         {
             IWebElement cssElement = myDriver.FindElement(By.CssSelector(strToFindBy));
+            Assert.That(cssElement.Displayed, Is.True, "Element in not dosplayed!");
             cssElement.Click();
         }
 
         public static void FindByXpathAndClick(IWebDriver myDriver, string strToFindBy)
         {
-            IWebElement cssElement = myDriver.FindElement(By.XPath(strToFindBy));
-            cssElement.Click();
+            IWebElement xPathElement = myDriver.FindElement(By.XPath(strToFindBy));
+            Assert.That(xPathElement.Displayed, Is.True, "Element is not displayed!");
+            xPathElement.Click();
         }
 
 
